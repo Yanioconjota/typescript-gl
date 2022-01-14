5314737eb0b40494b7ba849dcbec43338e938ef8
Janio Isacura <janioisacura@gmail.com>
2022-01-13 21:56:38 -0300

    New types examples added

---
+++ 02-moreTypes/src/app.ts
@@ -0,0 +1,140 @@
+interface OAdmin {
+  name: string;
+  privileges: string[];
+}
+
+interface OEmployee {
+  name: string;
+  startDate: Date;
+}
+
+interface OElevatedEmployee extends OAdmin, OEmployee {}
+
+const oe1: OElevatedEmployee = {
+  name: 'Homero Simpson',
+  privileges: ['create-server'],
+  startDate: new Date()
+};
+
+console.log(oe1);
+
+//Intersection Type --> Shorthand del código anterior
+
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+}
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Homero Thompson',
+  privileges: ['create-server'],
+  startDate: new Date()
+};
+
+console.log(e1);
+
+type Combinable = string | number; //Union: uno u otro
+type Numeric = Combinable | number; //Union: uno u otro
+type Universal = Combinable & Numeric; //Intersection: ambos
+
+//Union vs Intersection
+
+//Union: que recibe un valor que puede ser de un tipo u otro
+//Intersection: combina multiples tipos en uno solo.
+//https://medium.com/@Methrat0n/union-types-and-intersection-types-50c41c9b61d6
+
+//Partial<Type> --> construye un tipo constituido por propiedades opcionales 
+
+interface Todo {
+  title: string;
+  description: string;
+}
+ 
+const updateTodo = (todo: Todo, fieldsToUpdate: Partial<Todo>) => {
+  return { ...todo, ...fieldsToUpdate };
+}
+ 
+const todo1 = {
+  title: "organize desk",
+  description: "clear clutter",
+};
+ 
+const todo2 = updateTodo(todo1, {
+  description: "throw out trash",
+});
+
+const todo3 = updateTodo(todo2, {title: 'Fart in public'});
+
+console.log(todo2);
+console.log(todo3);
+
+//Required<Type> --> Al contrario de Partial construye un tipo cuyas propiedades son todas requeridas
+
+interface Props {
+  a?: number;
+  b?: string;
+}
+ 
+const obj: Props = { a: 5 };
+ 
+//const obj2: Required<Props> = { a: 5 };//Property 'b' is missing in type '{ a: number; }' but required in type 'Required<Props>'.
+
+//Readonly<Type> Construye un tipo donde sus propiedades son solo de lectura, por ende, no pueden ser reasignadas
+
+interface Message {
+  title: string;
+}
+ 
+const msg: Readonly<Message> = {
+  title: "Delete inactive users",
+};
+ 
+//msg.title = "Hello"; //Cannot assign to 'title' because it is a read-only property.
+
+//function freeze<Type>(obj: Type): Readonly<Type>;
+
+//Record<Keys, Type> --> Se utiliza para mapear las propiedades de un tipo dentro de otro tipo
+
+interface DogInfo {
+  age: number;
+  breed: string
+};
+
+type DogName = 'Morgan' | 'Milo' | 'Negrito';
+
+const dogs: Record<DogName, DogInfo> = {
+  Morgan: { age: 3, breed: 'Stray'},
+  Milo: { age: 7, breed: 'French Poodle' },
+  Negrito: { age: 15, breed: 'Retriever' }
+}
+
+//Iterar con objetos en typescript: https://trungk18.com/experience/how-to-iterate-over-objects-in-typescript/
+let dog: DogName;
+
+for (dog in dogs) {
+  console.log(dog, dogs[dog]);
+}
+
+//Pick<Type, Keys> --> Construye un tipo eligiendo set de valores 'keys' (string literals o union string literals) de un determinado tipo.
+
+interface TextMessage {
+  title: string;
+  description: string;
+  sent: boolean
+}
+
+type MessagePreview = Pick<TextMessage, "title" | "sent">
+
+const msg1: MessagePreview = {
+  title: 'Hola Morgan!',
+  sent: true
+};
+
+console.log(msg1);
